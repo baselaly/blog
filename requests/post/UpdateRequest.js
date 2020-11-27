44b6c3a35ace35aecea3b4a46398d05e49bf1201
basel <basel.3ly@gmail.com>
2020-11-27 20:54:40 +0200

    update post edit for image upload

--- requests/post/UpdateRequest.js
+++ requests/post/UpdateRequest.js
@@ -1,6 +1,47 @@
-const { check } = require('express-validator');
+const { checkSchema } = require('express-validator');
 
-module.exports = [
-    check('title').exists().isLength({ max: 200 }),
-    check('description').exists().isLength({ max: 5000 })
-]
+module.exports = checkSchema({
+    title: {
+        in: ['body'],
+        trim: true,
+        notEmpty: {
+            errorMessage: "title field is required"
+        },
+        isLength: {
+            options: {
+                max: 200,
+            },
+            errorMessage: "title field must be less than 200 chars"
+        },
+    },
+    description: {
+        in: ['body'],
+        trim: true,
+        notEmpty: {
+            errorMessage: "description field is required"
+        },
+        isLength: {
+            options: {
+                max: 1000,
+            },
+            errorMessage: "description field must be less than 1000 chars"
+        },
+    },
+    image: {
+        custom: {
+            options: (value, { req }) => {
+                if (req.file) {
+                    if (req.file.mimetype !== 'image/jpeg' && req.file.mimetype !== 'image/jpg' && req.file.mimetype !== 'image/png') {
+                        return Promise.reject("Image Field is must be type jpg/jpeg/png")
+                    }
+                    if (req.file.size > 1024 * 1024 * 5) {
+                        return Promise.reject("Image is must be lessthan 5 mb")
+                    }
+                    return true
+                } else {
+                    return true
+                }
+            }
+        }
+    }
+})
--- routes/posts.js
+++ routes/posts.js
@@ -6,9 +6,9 @@ const StoreRequest = require('../requests/post/StoreRequest')
 const UpdateRequest = require('../requests/post/UpdateRequest')
 const uploadMiddleware = require('../config/file.upload')
 
-router.post('/', uploadMiddleware.single('image'), StoreRequest, PostController.store);
+router.post('/', authMiddleware, uploadMiddleware.single('image'), StoreRequest, PostController.store);
 router.get('/', authMiddleware, PostController.index);
 router.get('/:postId', authMiddleware, PostController.view);
-router.post('/update/:postId', UpdateRequest, authMiddleware, PostController.update);
+router.post('/update/:postId', authMiddleware, uploadMiddleware.single('image'), UpdateRequest, PostController.update);
 
 module.exports = router
